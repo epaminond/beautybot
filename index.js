cfac0e062a4f80f78e46052e5f464d4ed257053b
Epaminond <k4merton@gmail.com>
2017-06-04 20:45:07 +0300

    Answer questions regarding beauty services :telephone_receiver:
    https://trello.com/c/3VsTj2Ar/3-answer-questions-about-the-services-offered-by-the-salon

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+const services = require('./services')
+
 module.exports = function(bp) {
   bp.middlewares.load()
 
-  // Listens for a message (this is a Regex)
-  // GET_STARTED is the first message you get on Facebook Messenger
-  bp.hear(/GET_STARTED|hello|hi|hey/i, (event, next) => {
-    bp.messenger.sendText(event.user.id, 'welcome!!')
+  bp.hear(/\bhello\b|\bhi\b|\bhey\b/i, (event, next) => {
+    bp.messenger.sendText(event.user.id, 'Hi! I\'m a Beauty Salon Bot! Is there anything I could help you with?')
+  })
+
+  bp.hear(/.*services.*/i, (event, next) => {
+    const serviceNames = Object.values(services).map(service => service.name)
+    bp.messenger.sendText(event.user.id, `Here's a list of our services: ${serviceNames.join(', ')}.`)
+  })
+
+  Object.values(services).map(service => {
+    const match = new RegExp(`about ${service.name}|price of ${service.name}|${service.name} price`, 'i')
+    bp.hear(match, (event, next) => {
+      bp.messenger.sendText(event.user.id, `We are doing ${service.name} pretty good.
+                                            ${service.description}.
+                                            The price is $${service.price}.`)
+    })
   })
 }
---
+++ services.js
@@ -0,0 +1,18 @@
+module.exports = {
+  haircut: {
+    name: 'Haircut',
+    description: 'We are doing the most beautiful haircuts you could ever imagine',
+    price: 100,
+  },
+  manicure: {
+    name: 'Manicure',
+    description: 'Manicure includes file and cuticle tidy, light exfoliation, warm paraffin, massage, \
+                  buff and polish change',
+    price: 50,
+  },
+  detox: {
+    name: 'Detox',
+    description: 'What we\'ll do is detoxing, smoothing and toning your skin with essential oils',
+    price: 200,
+  }
+}

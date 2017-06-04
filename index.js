3709c8e928213f6e642c9a061ddb1340c6de3fa6
Epaminond <k4merton@gmail.com>
2017-06-04 23:31:38 +0300

    Shocase services

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const services = require('./services')
+const { services, portfolio } = require('./fixtures')
 
 module.exports = function(bp) {
   bp.middlewares.load()
 
+  // Hello-goodbye
   bp.hear(/\bhello\b|\bhi\b|\bhey\b/i, (event, next) => {
     bp.messenger.sendText(event.user.id, 'Hi! I\'m a Beauty Salon Bot! Is there anything I could help you with?')
   })
 
+  bp.hear(/\bbye\b/i, (event, next) => {
+    bp.messenger.sendText(event.user.id, 'See you! :)')
+  })
+
+  // Inquiries on services
   bp.hear(/.*services.*/i, (event, next) => {
     const serviceNames = Object.values(services).map(service => service.name)
     bp.messenger.sendText(event.user.id, `Here's a list of our services: ${serviceNames.join(', ')}.`)
@@ -21,10 +27,18 @@ module.exports = function(bp) {
     })
   })
 
-  bp.hear(/\bbye\b/i, (event, next) => {
-    bp.messenger.sendText(event.user.id, 'See you! :)')
+  bp.hear(/.*services.*/i, (event, next) => {
+    const serviceNames = Object.values(services).map(service => service.name)
+    bp.messenger.sendText(event.user.id, `Here's a list of our services: ${serviceNames.join(', ')}.`)
   })
 
+  // Showcase
+  bp.hear(/.*showcase.*|.*examples.*|.*portfolio.*/i, (event, next) => {
+    bp.messenger.sendText(event.user.id, `Check some of our works below`)
+    bp.messenger.sendTemplate(event.user.id, { template_type: 'generic', elements: portfolio })
+  })
+
+  // Answer unhandled messages
   bp.hear(/.+/, (event, next) => {
     if(event.type !== 'message') { return }
     bp.messenger.sendText(event.user.id, 'Sorry, I didn\'t quite get you.')
